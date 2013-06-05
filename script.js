03e97658bb18a6ce74ffc30dfd69346a8147fbc9
Klap-in <klapinklapin@gmail.com>
2013-06-05 11:29:37 +0200

    added some inline javascript from action and syntax component code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function book_changePage(name, value, expires, path, domain, secure) {
     var curCookie = name + "=" + encodeURIComponent(value) +
         ((expires) ? "; expires=" + expires.toGMTString() : "") +
@@ -21,7 +22,7 @@ function book_getPage(name) {
     return decodeURIComponent(dc.substring(begin + prefix.length, end));
 }
 
-// cancella la pagina della selezione
+// clears the selection page
 function book_removeAllPages(rootname) {
 
     var cookie_date = new Date();  // current date & time
@@ -47,7 +48,12 @@ function book_removeAllPages(rootname) {
 }
 
 
-// conta le pagine presenti
+/**
+ * counts the pages
+ *
+ * @param rootname
+ * @return {Number} number of pages
+ */
 function book_countPages(rootname) {
     var tmp = "";
     var n = -1;
@@ -71,8 +77,66 @@ function book_countPages(rootname) {
     return k;
 }
 
-// ricarica la pagina
+/**
+ * reload the page
+ */
 function book_recharge() {
     window.location.reload();
+}
+
+/**
+ * Handle read or delete of a Selection from the Selection List
+ *
+ * @param {string} action 'del' or 'read'
+ * @param {string} page name of page with the list of selected pages
+ * @return {Boolean}
+ */
+function actionList(action,page) {
+    var msg = "";
+    var flag = true;
+    var flagconfirm = true;
+    if (action == "del") {
+        msg = LANG.plugins.bookcreator.confirmdel;
+    } else {
+        if (book_countPages("bookcreator") == 0) {
+            flag = false;
+        }
+        msg = LANG.plugins.bookcreator.confirmload;
+    }
+
+    if (flag) flagconfirm = confirm(msg);
+    if(flagconfirm) {
+        document.bookcreator__selections__list.task.value=action;
+        document.bookcreator__selections__list.page.value=page;
+        document.bookcreator__selections__list.submit();
+        return true;
+    }
+}
+
+/**
+ * Toggle element with given id
+ *
+ * @param {string} id of the element
+ */
+function book_revertLink(id) {
+    var elem = jQuery("#"+id)[0];
+    if (document && elem)
+        if (elem.style.display=="block")
+            elem.style.display="none";
+        else
+            elem.style.display="block";
+}
+
+/**
+ * Update cookie and toggle the add/remove buttons in toolbar
+ *
+ * @param {string} id    pageid
+ * @param {int}    value 1=add, 0=remove
+ */
+function book_updateSelection(id, value) {
+    book_changePage("bookcreator["+id+"]", value, new Date("July 21, 2099 00:00:00"), "/");
+    book_revertLink("bookcreator__remove");
+    book_revertLink("bookcreator__add");
+    jQuery("#bookcreator__pages")[0].innerHTML= book_countPages("bookcreator");
 }
 
