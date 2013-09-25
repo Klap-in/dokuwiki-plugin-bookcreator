9814bcd2c7321713f41f3f0263597923ad56ea9c
ronny <el_cherone@airpost.net>
2013-09-25 14:10:05 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,48 +1,44 @@
 <?php
-$lang['nocookies']       = "Sie haben noch keine Seiten gewählt. Bitte überprüfen Sie, ob ihr Browser Cookies erlaubt und versuchen Sie es erneut.";
-$lang['empty']           = "Sie haben noch keine Seiten gewählt.";
-$lang['toprint']         = "Ausgewählte Seiten";
-$lang['removed']         = "Seiten, die aus der Auswahl entfernt wurden";
-$lang['reserconfirm']    = "Wollen Sie die Auswahl wirklich löschen?";
-$lang['reset']           = "Auswahl löschen";
-$lang['js']['include']   = "Zur Auswahl hinzufügen";
-$lang['js']['remove']    = "Seite aus Auswahl entfernen";
-$lang['export']          = "Ausgewählte Seiten exportieren";
-$lang['create']          = "Exportieren";
 
-$lang['exportpdf']       = "Auswahl als PDF exportieren";
-$lang['exportprint']     = "Druckversion";
-$lang['exporttext']      = "Textversion";
-
-$lang['title']           = "Titel des Dokuments (optional)";
-
-$lang['pageadded']       = "Zur Auswahl hinzugefügte Seite/n";
-$lang['pageremoved']     = "Aus Auswahl entfernte Seite";
-
-$lang['addpage']         = "Diese Seite zum Buch hinzufügen";
-$lang['removepage']      = "Diese Seite aus Buch entfernen";
-$lang['showbook']        = "Buch anzeigen, ändern";
-$lang['sortable']        = "Drag and drop to sort/remove/add pages";
-
-$lang['pages']           = "Seite/n";
-$lang['help']            = "Hilfe";
-
-$lang['toolbar']         = "Buch erstellen";
-/*neu*/
-$lang['saveselection']   = "Auswahl speichern";
-$lang['needtitle']       = "Bitte Titel angeben!";
-$lang['saved']           = "Auswahl erfolgreich gespeichert";
-$lang['deleted']         = "Auswahl erfolgreich entfernt";
-$lang['duplicate']       = "&lt;The list with saved selections is already displayed on this page&gt;";
-
-$lang['save']            = "Speichern";
-$lang['listselections']  = "Liste der abgespeicherten Auswahlen";
-
-$lang['delselection']    = "Entferne gespeicherte Auswahl";
-$lang['showpage']        = "Zeige Seite";
-$lang['loadselection']   = "Lade gespeicherte Liste mit Auswahl";
-
-$lang['js']['confirmdel']      = "Diese Auswahl aus dem Archiv löschen?";
-$lang['js']['confirmload']     = "Wollen Sie diese Auswahl laden? Damit werden bereits ausgwählte Seiten ersetzt!";
-$lang['js']['btn_addtobook']    = "Add to book";
-$lang['js']['btn_removetobook'] = "Remove from book";
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author ronny <el_cherone@airpost.net>
+ */
+$lang['nocookies']             = 'Sie haben noch keine Seiten gewählt. Bitte überprüfen Sie, ob ihr Browser Cookies erlaubt und versuchen Sie es erneut.';
+$lang['empty']                 = 'Sie haben noch keine Seiten gewählt.';
+$lang['toprint']               = 'Ausgewählte Seiten';
+$lang['removed']               = 'Seiten, die aus der Auswahl entfernt wurden';
+$lang['reserconfirm']          = 'Wollen Sie die Auswahl wirklich löschen?';
+$lang['reset']                 = 'Auswahl löschen';
+$lang['js']['include']         = 'Zur Auswahl hinzufügen';
+$lang['js']['remove']          = 'Seite aus Auswahl entfernen';
+$lang['js']['confirmdel']      = 'Diese Auswahl aus dem Archiv löschen?';
+$lang['js']['confirmload']     = 'Wollen Sie diese Auswahl laden? Damit werden bereits ausgwählte Seiten ersetzt!';
+$lang['js']['btn_addtobook']   = 'Zu Buch hinzufügen';
+$lang['js']['btn_removetobook'] = 'Von Buch entfernen';
+$lang['export']                = 'Ausgewählte Seiten exportieren';
+$lang['create']                = 'Exportieren';
+$lang['exportpdf']             = 'Auswahl als PDF exportieren';
+$lang['exportprint']           = 'Druckversion';
+$lang['exporttext']            = 'Textversion';
+$lang['title']                 = 'Titel des Dokuments (optional)';
+$lang['pageadded']             = 'Zur Auswahl hinzugefügte Seite/n';
+$lang['pageremoved']           = 'Aus Auswahl entfernte Seite';
+$lang['addpage']               = 'Diese Seite zum Buch hinzufügen';
+$lang['removepage']            = 'Diese Seite aus Buch entfernen';
+$lang['showbook']              = 'Buch anzeigen, ändern';
+$lang['sortable']              = 'Drag und drop um Seiten zu sortieren/entfernen/hinzufügen';
+$lang['pages']                 = 'Seite/n';
+$lang['help']                  = 'Hilfe';
+$lang['toolbar']               = 'Buch erstellen';
+$lang['saveselection']         = 'Auswahl speichern';
+$lang['needtitle']             = 'Bitte Titel angeben!';
+$lang['saved']                 = 'Auswahl erfolgreich gespeichert';
+$lang['deleted']               = 'Auswahl erfolgreich entfernt';
+$lang['duplicate']             = '&lt;Die Liste mit der gespeicherten Auswahl wird bereits auf dieser Seite angezeigt&gt;';
+$lang['save']                  = 'Speichern';
+$lang['listselections']        = 'Liste der abgespeicherten Auswahl';
+$lang['showpage']              = 'Zeige Seite';
+$lang['delselection']          = 'Entferne gespeicherte Auswahl';
+$lang['loadselection']         = 'Lade gespeicherte Liste mit Auswahl';
