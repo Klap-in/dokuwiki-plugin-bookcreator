43bcb0ff00e57995a76c938ab7091dd7fb65a01d
Myeongjin <aranet100@gmail.com>
2013-10-16 04:40:05 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['nocookies']             = '아직 선택한 페이지가 없습니다. 쿠키를 허용하도록 브라우저를 확인하고 다시 시도하세요.';
+$lang['empty']                 = '아직 선택한 페이지가 없습니다.';
+$lang['toprint']               = '선택한 페이지';
+$lang['removed']               = '선택에서 페이지가 제거됨';
+$lang['reserconfirm']          = '선택을 지우겠습니까 ?';
+$lang['reset']                 = '선택 지우기';
+$lang['js']['include']         = '선택에 추가';
+$lang['js']['remove']          = '선택에서 페이지 제거';
+$lang['js']['confirmdel']      = '아카이브에서 이 선택을 삭제하겠습니까?';
+$lang['js']['confirmload']     = '이미 선택한 문서를 바꾸기 위해 이 선택을 불러오겠습니까?';
+$lang['js']['btn_addtobook']   = '책에 추가';
+$lang['js']['btn_removetobook'] = '책에서 제거';
+$lang['export']                = '선택 내보내기';
+$lang['create']                = '내보내기';
+$lang['exportpdf']             = 'PDF로 선택 내보내기';
+$lang['exportprint']           = '인쇄용 문서';
+$lang['exporttext']            = '텍스트만';
+$lang['title']                 = '문서의 제목 (선택 사항)';
+$lang['pageadded']             = '선택으로 페이지 추가';
+$lang['pageremoved']           = '선택에서 페이지 제거됨';
+$lang['addpage']               = '내 책으로 이 페이지 추가';
+$lang['removepage']            = '내 책에서 이 페이지 제거';
+$lang['showbook']              = '책 수적을 보이기';
+$lang['sortable']              = '문서를 정렬/제거/추가하려면 끌어 놓으세요';
+$lang['pages']                 = '문서';
+$lang['help']                  = '도움말';
+$lang['toolbar']               = '책 생성기';
+$lang['saveselection']         = '선택 저장';
+$lang['needtitle']             = '제목을 제공하세요';
+$lang['saved']                 = '선택을 성공적으로 저장했습니다';
+$lang['deleted']               = '선택을 성공적으로 제거했습니다.';
+$lang['duplicate']             = '&lt;선택이 저장된 목록은 이미 이 문서에 보여지고 있습니다&gt;';
+$lang['save']                  = '저장';
+$lang['listselections']        = '선택 목록 저장됨';
+$lang['showpage']              = '문서 보기';
+$lang['delselection']          = '저장된 선택 제거';
+$lang['loadselection']         = '저장된 선택 불러오기';
---
+++ lang/ko/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['toolbar']               = '도구막대 보이기';
+$lang['toolbar_o_never']       = '없음';
+$lang['toolbar_o_always']      = '항상';
+$lang['toolbar_o_noempty']     = '선택이 비어 있지 않을 때';
+$lang['save_namespace']        = '선택을 저장할 이름공간';
+$lang['book_page']             = '내 책을 보여줄 위키 문서';
+$lang['help_page']             = '정보와 지시로 된 위키 문서';
+$lang['skip_ids']              = 'ID가 포함될 때 도구를 보이지 않음';
