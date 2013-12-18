a029cabe2d9ecaaf9eaafdd4db6dd279d9159a90
Hideaki SAWADA <chuno@live.jp>
2013-12-18 13:10:40 +0100

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['toolbar']               = 'ツールバーの表示';
+$lang['toolbar_o_never']       = 'しない';
+$lang['toolbar_o_always']      = '常に';
+$lang['toolbar_o_noempty']     = '選択が空でない場合';
+$lang['save_namespace']        = '選択を保存する名前空間';
+$lang['book_page']             = 'ブックを表示するウィキページ';
+$lang['help_page']             = '情報と手順のウィキページ';
+$lang['skip_ids']              = 'IDが含まれる場合、ツールは表示しない';
