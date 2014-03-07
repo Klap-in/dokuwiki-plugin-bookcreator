db5ddb58536d057820ab646776e19d686261575b
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-03-07 10:20:05 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,13 +1,15 @@
 <?php
- 
-$lang['toolbar']             = "Montrer la barre d'outil";
-$lang['toolbar_o_never']     = "Jamais";
-$lang['toolbar_o_always']    = "Toujours";
-$lang['toolbar_o_noempty']   = "Quand la sélection n'est pas vide";
 
-$lang['save_namespace']      = 'Namespace o� enregistrer vos sélections';
-
-$lang['book_page']           = "Page Wiki o� afficher votre livre";
-$lang['help_page']           = "Page Wiki avec les informations et les instructions";
- 
-$lang['skip_ids']            = "Ne pas afficher les outils quand l'ID contient";
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['toolbar']               = 'Montrer la barre d\'outil';
+$lang['toolbar_o_never']       = 'Jamais';
+$lang['toolbar_o_always']      = 'Toujours';
+$lang['toolbar_o_noempty']     = 'Quand la sélection n\'est pas vide';
+$lang['save_namespace']        = 'Catégorie où enregistrer vos sélections';
+$lang['book_page']             = 'Page Wiki où afficher votre gestionnaire de livre';
+$lang['help_page']             = 'Page Wiki avec les informations et les instructions';
+$lang['skip_ids']              = 'Ne pas afficher les outils quand l\'ID contient';
