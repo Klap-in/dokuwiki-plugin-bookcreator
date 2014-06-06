020a213a6a1f3d40b2bebb9f734e94bf144835cf
Gerrit Uitslag <klapinklapin@gmail.com>
2014-06-06 12:20:45 +0200

    add language hr

---
+++ lang/hr/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Croatian language strings for the bookcreator Plugin
+ *
+ * translated by  Davor Turkalj <turki.bsc@gmail.com>
+ */
+
+$lang['toolbar']             = 'Prikaži info traku';
+$lang['toolbar_o_never']     = "Nikad";
+$lang['toolbar_o_always']    = "Uvijek";
+$lang['toolbar_o_noempty']   = "Ako lista odabira nije prazna";
+
+$lang['save_namespace']      = 'Imenički prostor gdje se snimaju odabiri'; 
+
+$lang['book_page']           = 'Wiki stranica gdje je knjiški upravitelj';
+$lang['help_page']           = 'Wiki stranica s informacijama i uputama';
+
+$lang['skip_ids']            = 'Ne prikazuj info traku ako ID stranice sadrži';
+
