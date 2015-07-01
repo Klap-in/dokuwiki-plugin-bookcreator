4fbde2b7b183504788e03af03d72613856caf1c9
Davor Turkalj <turki.bsc@gmail.com>
2015-07-01 12:30:18 +0200

    translation update

--- lang/hr/settings.php
+++ lang/hr/settings.php
@@ -1,19 +1,14 @@
 <?php
+
 /**
- * Croatian language strings for the bookcreator Plugin
- *
- * translated by  Davor Turkalj <turki.bsc@gmail.com>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  */
-
-$lang['toolbar']             = 'Prikaži info traku';
-$lang['toolbar_o_never']     = "Nikad";
-$lang['toolbar_o_always']    = "Uvijek";
-$lang['toolbar_o_noempty']   = "Ako lista odabira nije prazna";
-
-$lang['save_namespace']      = 'Imenički prostor gdje se snimaju odabiri'; 
-
-$lang['book_page']           = 'Wiki stranica gdje je knjiški upravitelj';
-$lang['help_page']           = 'Wiki stranica s informacijama i uputama';
-
-$lang['skip_ids']            = 'Ne prikazuj info traku ako ID stranice sadrži';
-
+$lang['toolbar']               = 'Prikaži info traku';
+$lang['toolbar_o_never']       = 'Nikad';
+$lang['toolbar_o_always']      = 'Uvijek';
+$lang['toolbar_o_noempty']     = 'Ako lista odabira nije prazna';
+$lang['save_namespace']        = 'Imenički prostor gdje se snimaju odabiri';
+$lang['book_page']             = 'Wiki stranica gdje je knjiški upravitelj';
+$lang['help_page']             = 'Wiki stranica s informacijama i uputama';
+$lang['skip_ids']              = 'Ne prikazuj info traku ako ID stranice sadrži';
