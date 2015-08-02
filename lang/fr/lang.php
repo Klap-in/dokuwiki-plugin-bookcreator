ad34999bceb30fda89a483955dccc9e714d3733f
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2015-08-02 14:40:06 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
 $lang['nocookies']             = 'Vous n\'avez pas encore sélectionné de pages. Vérifier que votre navigateur accepte les cookies et essayez de nouveau.';
 $lang['empty']                 = 'Vous n\'avez pas encore sélectionné de pages.';
@@ -20,8 +21,9 @@ $lang['js']['btn_removetobook'] = 'Retirer du livre.';
 $lang['export']                = 'Exporter la sélection.';
 $lang['create']                = 'Exporter';
 $lang['exportpdf']             = 'Exporter en PDF';
+$lang['exportodt']             = 'Exporter la sélection en ODT.';
 $lang['exportprint']           = 'Version imprimable';
-$lang['exporttext']            = 'Version seul texte';
+$lang['exporttext']            = 'Version texte seul';
 $lang['title']                 = 'Titre du document (optionnel)';
 $lang['pageadded']             = 'Page ajoutée à la sélection';
 $lang['pageremoved']           = 'Page retirée de la sélection';
@@ -36,7 +38,7 @@ $lang['saveselection']         = 'Enregistrer la sélection';
 $lang['needtitle']             = 'Il faut indiquer un titre.';
 $lang['saved']                 = 'Sélection enregistrée avec succès.';
 $lang['deleted']               = 'Sélection supprimée avec succès.';
-$lang['duplicate']             = '&lt;La liste des sélections sauvées est déjà affichée sur cette page.&gt>';
+$lang['duplicate']             = '&lt;La liste des sélections sauvées est déjà affichée sur cette page.&gt;';
 $lang['save']                  = 'Enregistrer';
 $lang['listselections']        = 'Liste des sélections enregistrées';
 $lang['showpage']              = 'Afficher la page.';
