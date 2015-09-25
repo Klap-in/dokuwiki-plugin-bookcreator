39d6bbc7598714373ba7d472b28b6101dd7a6257
Mario <mariodiaz69@gmail.com>
2015-09-25 16:20:06 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -6,15 +6,16 @@
  * @author Luigi Micco <l.micco@tiscali.it>
  * @author Eloy <ej.perezgomez@gmail.com>
  * @author Henrique <henrifc@gmail.com>
+ * @author Mario <mariodiaz69@gmail.com>
  */
 $lang['nocookies']             = 'La lista está vacía. Verifique que su explorador permite el uso de cookies y vuelva a intentarlo.';
 $lang['empty']                 = 'La lista está vacía.';
 $lang['toprint']               = 'Páginas seleccionadas';
 $lang['removed']               = 'Páginas eliminadas de la selección';
 $lang['reserconfirm']          = '¿Realmente quiere eliminar la selección?';
-$lang['reset']                 = 'Eliminar selección';
+$lang['reset']                 = 'Quitar selección';
 $lang['js']['include']         = 'Añadir a la selección';
-$lang['js']['remove']          = 'Eliminar de la selección';
+$lang['js']['remove']          = 'Quitar página de la selección';
 $lang['js']['confirmdel']      = '¿Realmente quiere eliminar esta selección del archivo?';
 $lang['js']['confirmload']     = '¿Realmente quiere cargar esta selección, remplazando las páginas seleccionadas actualmente?';
 $lang['js']['btn_addtobook']   = 'Añadir al libro';
@@ -24,7 +25,7 @@ $lang['create']                = 'Exportar';
 $lang['exportpdf']             = 'Exportar selección a PDF';
 $lang['exportodt']             = 'Esportar la selección a ODT';
 $lang['exportprint']           = 'Versión imprimible';
-$lang['exporttext']            = 'Solo texto';
+$lang['exporttext']            = 'Sólo texto';
 $lang['title']                 = 'Título del documento (opcional)';
 $lang['pageadded']             = 'Página añadida a la selección';
 $lang['pageremoved']           = 'Página eliminada de la selección';
@@ -34,15 +35,15 @@ $lang['showbook']              = 'Mostrar, modificar libro';
 $lang['sortable']              = 'Arrastrar y soltar para ordenar / quitar / añadir páginas';
 $lang['pages']                 = 'página/s';
 $lang['help']                  = 'Ayuda';
-$lang['toolbar']               = 'Creación de libros';
+$lang['toolbar']               = 'Creador de libros';
 $lang['saveselection']         = 'Guardar selección';
 $lang['needtitle']             = 'Debe asignar un título';
 $lang['saved']                 = 'Selecciones guardadas correctamente';
 $lang['deleted']               = 'Selecciones eliminadas correctamente';
-$lang['duplicate']             = '&lt;The list with saved selections is already displayed on this page&gt;';
+$lang['duplicate']             = '&lt;La lista con la selección guardada ya se muestra en esta página&gt;';
 $lang['save']                  = 'Guardar';
 $lang['listselections']        = 'Lista de selección guardada';
 $lang['showpage']              = 'Mostrar página';
 $lang['delselection']          = 'Eliminar selección guardada';
 $lang['loadselection']         = 'Cargar selección guardada';
-$lang['nobookmanageraccess']   = 'No puedes cambiar tu selección. Solo se permite cuando tienes acceso al gestor de libros.';
+$lang['nobookmanageraccess']   = 'No puedes cambiar tu selección. Sólo se permite cuando tienes acceso al gestor de libros.';
