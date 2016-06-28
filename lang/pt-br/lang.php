fafeaec5e3681b1605b80b83f8157862473521fd
Felipe Castro <fefcas@gmail.com>
2016-06-28 14:20:05 +0200

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,49 +1,46 @@
 <?php
 
-$lang['nocookies']       = "Você não tem páginas selecionadas. Verifique se o seu navegador para permitir cookies e tente novamente.";
-$lang['empty']           = "Você não tem páginas selecionadas.";
-$lang['toprint']         = "Páginas selecionadas";
-$lang['removed']         = "Páginas removidas da seleção";
-
-$lang['reserconfirm']    = "Você quer realmente limpar a seleção feita?";
-$lang['reset']           = "Limpar seleção";
-
-$lang['js']['include']         = "Adicionar à seleção";
-$lang['js']['remove']          = "Remova a página da seleção";
-$lang['export']          = "Exportar seleção";
-$lang['create']          = "Exportar";
-
-$lang['exportpdf']       = "Exportar seleção para PDF";
-$lang['exportprint']     = "Versão para impressão";
-$lang['exporttext']      = "Apenas texto";
-
-$lang['title']           = "Título do documento (opcional)";
-
-$lang['pageadded']       = "Adicionar página à seleção";
-$lang['pageremoved']     = "Excluir página da seleção";
-
-$lang['addpage']         = "Adicionar esta página ao seu livro";
-$lang['removepage']      = "Remover esta página de seu livro";
-$lang['showbook']        = "Mostrar, modificar livro";
-$lang['sortable']        = "Drag and drop to sort/remove/add pages";
-
-$lang['pages']           = "páginas";
-$lang['help']            = "Ajuda'";
-
-$lang['toolbar']         = "Criação de Livros";
-
-$lang['saveselection']   = "Salvar seleção";
-$lang['needtitle']       = "Deve indicar um título";
-$lang['saved']           = "Seleção foi salva com sucesso";
-$lang['deleted']         = "Seleção foi excluída com sucesso";
-$lang['duplicate']       = "&lt;The list with saved selections is already displayed on this page&gt;";
-
-$lang['save']            = "Salvar";
-$lang['listselections']  = "Lista de seleção salva";
-
-$lang['delselection']    = "Remover a seleção salva";
-$lang['showpage']        = "Mostrar página";
-$lang['loadselection']   = "Carregar seleção salva";
-
-$lang['js']['confirmdel']      = "Você realmente quer excluir esta seleção do arquivo?";
-$lang['js']['confirmload']     = "Você realmente quer carregar esta seleção, substituindo todas as páginas já selecionados?";
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Felipe Castro <fefcas@gmail.com>
+ */
+$lang['nocookies']             = 'Você não tem páginas selecionadas. Verifique se o seu navegador para permitir cookies e tente novamente.';
+$lang['empty']                 = 'Você ainda não tem páginas selecionadas.';
+$lang['toprint']               = 'Páginas selecionadas';
+$lang['removed']               = 'Páginas removidas da seleção';
+$lang['reserconfirm']          = 'Você quer realmente limpar a seleção feita?';
+$lang['reset']                 = 'Limpar seleção';
+$lang['js']['include']         = 'Adicionar à seleção';
+$lang['js']['remove']          = 'Remova a página da seleção';
+$lang['js']['confirmdel']      = 'Você realmente quer excluir esta seleção do arquivo?';
+$lang['js']['confirmload']     = 'Você realmente quer carregar esta seleção, substituindo todas as páginas já selecionados?';
+$lang['js']['btn_addtobook']   = 'Adicionar a livro';
+$lang['js']['btn_removetobook'] = 'Remover de livro';
+$lang['export']                = 'Exportar seleção';
+$lang['create']                = 'Exportar';
+$lang['exportpdf']             = 'Exportar seleção para PDF';
+$lang['exportodt']             = 'Exportar seleção para ODT';
+$lang['exportprint']           = 'Versão para impressão';
+$lang['exporttext']            = 'Apenas texto';
+$lang['title']                 = 'Título do documento (opcional)';
+$lang['pageadded']             = 'Adicionar página à seleção';
+$lang['pageremoved']           = 'Excluir página da seleção';
+$lang['addpage']               = 'Adicionar esta página ao seu livro';
+$lang['removepage']            = 'Remover esta página de seu livro';
+$lang['showbook']              = 'Gerenciar livro';
+$lang['sortable']              = 'Pegue e arraste para ordenar/remover/adicionar páginas';
+$lang['pages']                 = 'página(s)';
+$lang['help']                  = 'Ajuda';
+$lang['toolbar']               = 'Criação de livros';
+$lang['saveselection']         = 'Salvar seleção';
+$lang['needtitle']             = 'Deves indicar um título';
+$lang['saved']                 = 'Seleção foi salva com sucesso';
+$lang['deleted']               = 'Seleção foi excluída com sucesso';
+$lang['duplicate']             = '&lt;A lista com seleções salvas já está mostrada nesta página&gt;';
+$lang['save']                  = 'Salvar';
+$lang['listselections']        = 'Lista de seleção salva';
+$lang['showpage']              = 'Mostrar página';
+$lang['delselection']          = 'Remover a seleção salva';
+$lang['loadselection']         = 'Carregar seleção salva';
+$lang['nobookmanageraccess']   = 'Você não pode mudar a seleção. Somente é permitido quando você tiver acesso ao gerenciador de livros também.';
--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -1,18 +1,16 @@
 <?php
+
 /**
- * Português do Brasil strings para o Plugin "Pdfbook"
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author  Daniel Caixeta Queiroz Garcia <dcqg@hotmail.com>
+ * @author Daniel Caixeta Queiroz Garcia <dcqg@hotmail.com>
+ * @author Felipe Castro <fefcas@gmail.com>
  */
-
-$lang['toolbar']             = 'Mostrar barra de ferramentas';
-$lang['toolbar_o_never']     = "Nunca";
-$lang['toolbar_o_always']    = "Sempre";
-$lang['toolbar_o_noempty']   = "Quando a seleção não é vazia";
-
-$lang['save_namespace']      = 'Namespace where save selections';
-
-$lang['book_page']           = 'Página Wiki na qual será mostrado seu livro';
-$lang['help_page']           = 'Página Wiki contendo ajuda e informações';
-
-$lang['skip_ids']            = 'Não mostrar as ferramentas quando contém';
+$lang['toolbar']               = 'Mostrar barra de ferramentas';
+$lang['toolbar_o_never']       = 'Nunca';
+$lang['toolbar_o_always']      = 'Sempre';
+$lang['toolbar_o_noempty']     = 'Quando a seleção não é vazia';
+$lang['save_namespace']        = 'Espaço-de-nomes onde as seleções são salvas';
+$lang['book_page']             = 'Página Wiki na qual será mostrado seu livro';
+$lang['help_page']             = 'Página Wiki contendo ajuda e informações';
+$lang['skip_ids']              = 'Não mostrar as ferramentas quando o ID contém';
