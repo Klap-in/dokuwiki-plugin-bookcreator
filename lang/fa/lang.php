643bacaa92d460b78d1dc8db0e12bbe249566da4
Sam01 <m.sajad079@gmail.com>
2016-08-23 13:50:35 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['empty']                 = 'شما هیچ صفحه‌ای را انتخاب نکرده‌اید.';
+$lang['toprint']               = 'صفحه‌های انتخاب شده';
+$lang['removed']               = 'حذف صفحه‌ها از انتخاب شده';
+$lang['reset']                 = 'پاک کردن انتخاب شده';
+$lang['js']['include']         = 'اضافه کردن برای انتخاب';
+$lang['js']['remove']          = 'حذف صفحه از انتخاب شده‌';
+$lang['js']['sortable']        = 'کشیدن و رهاکردن برای مرتب‌سازی/حذف/اضافه کردن صفحه‌ها';
+$lang['js']['showpage']        = 'نمایش صفحه';
+$lang['js']['confirmdel']      = 'آیا واقعا می‌خواهید این انتخاب‌‌شده‌ها را از آرشیو حذف کنید؟';
+$lang['js']['confirmload']     = 'آیا واقعا می‌خواهید این انتخاب‌شده‌ها را برای جایگزین کردن صفحه‌های انتخاب شده کنونی بارگیری کنید؟';
+$lang['js']['btn_addtobook']   = 'اضافه کردن به کتاب';
+$lang['js']['btn_removetobook'] = 'حذف از کتاب';
+$lang['export']                = 'صادرکردن انتخابی‌ها';
+$lang['create']                = 'صادر کردن';
+$lang['exportpdf']             = 'صادر کردن انتخابی‌ها برای پی‌دی‌اف';
+$lang['exportodt']             = 'صادر کردن انتخابی‌ها برای سندباز';
+$lang['exportprint']           = 'نسخه قابل چاپ';
+$lang['exporttext']            = 'فقط نوشته';
+$lang['title']                 = 'عنوان سند';
+$lang['addpage']               = 'اضافه‌کردن این صفحه به کتاب‌تان';
+$lang['removepage']            = 'حذف‌کردن این صفحه از کتاب‌تان';
+$lang['showbook']              = 'مدیریت کتاب';
+$lang['pages']                 = 'صفحه(ها)';
+$lang['help']                  = 'کمک';
+$lang['toolbar']               = 'ساخت کتاب';
+$lang['saveselection']         = 'ذخیره انتخاب‌شده';
+$lang['needtitle']             = 'لطفا یک عنوان ارائه کنید';
+$lang['saved']                 = 'انتخاب‌شده‌ها با موفقیت ذخیره شدند: %s';
+$lang['deleted']               = 'انتخاب‌شده‌ها با موفقیت حذف شدند: %s';
+$lang['duplicate']             = '&lt;لیست همراه انتخاب‌شده‌های ذخیره‌ای درحال حاضر در این صفحه‌اند&gt;';
+$lang['save']                  = 'ذخیره';
+$lang['listselections']        = 'ذخیره‌ی لیست انتخابی';
+$lang['showselection']         = 'نمایش انتخاب‌شده ذخیره‌ای';
+$lang['delselection']          = 'حذف انتخاب‌شده ذخیره‌ای';
+$lang['loadselection']         = 'بارگیری انتخاب‌شده ذخیره‌ای';
+$lang['selectionstored']       = 'انتخاب‌های ذخیره شده';
+$lang['selectiondeleted']      = 'انتخاب‌شده حذف شد';
+$lang['selectiondontexist']    = 'انتخاب‌شده‌ای که در صفحه وجود ندارد \'%s\'';
+$lang['selectionforbidden']    = 'شما دسترسی برای انتخاب‌شده‌ها در صفحه را ندارید \'%s\'';
+$lang['titlepreparedownload']  = 'تعمیر دانلود...';
+$lang['preparingdownload']     = 'ما درحال تعمیر دانلود شما هستیم، شکیبا باشید...';
+$lang['titleerrordownload']    = 'خطا';
+$lang['faileddownload']        = 'یک مشکل در تولید دانلود شما وجود دارد.';
---
+++ lang/fa/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['toolbar']               = 'نمایش نوار ابزار';
+$lang['toolbar_o_never']       = 'هرگز';
+$lang['toolbar_o_always']      = 'همیشه';
+$lang['toolbar_o_noempty']     = 'زمانی که انتخابی خالی نیست';
+$lang['save_namespace']        = 'فضای‌نام درمحلی که انتخابی ذخیره شده';
+$lang['book_page']             = 'صفحه ویکی درمحلی که مدیریت کتاب نمایش داده شده';
+$lang['help_page']             = 'صفحه ویکی به همراه اطلاعات و دستورالعمل‌ها';
+$lang['skip_ids']              = 'نمایش ندادن ابزارها درمحل شامل آیدی';
