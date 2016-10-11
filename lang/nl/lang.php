bf4ba01faaaa48889754ebf693455bee0f35d1ff
klapinklapin <klapinklapin@gmail.com>
2016-10-11 22:30:05 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -5,6 +5,7 @@
  *
  * @author Rene <wllywlnt@yahoo.com>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
+ * @author klapinklapin <klapinklapin@gmail.com>
  */
 $lang['empty']                 = 'Er zijn nog geen pagina\'s geselecteerd.';
 $lang['toprint']               = 'Geselecteerde pagina\'s';
@@ -12,6 +13,8 @@ $lang['removed']               = 'Pagina\'s verwijderd uit de selectie';
 $lang['reset']                 = 'Verwijder selectie';
 $lang['js']['include']         = 'Toevoegen aan selectie';
 $lang['js']['remove']          = 'Verwijder pagina uit selectie';
+$lang['js']['sortable']        = 'Versleep de pagina\'s om ze te sorteren, verwijderen en weer toe te voegen';
+$lang['js']['showpage']        = 'Toon pagina';
 $lang['js']['confirmdel']      = 'Weet u zeker dat u deze selectie uit het archief wilt verwijderen?';
 $lang['js']['confirmload']     = 'Weet u zeker dat u deze selectie wilt laden en dus de huidige selectie wilt overschrijven?';
 $lang['js']['btn_addtobook']   = 'Voeg toe aan boek';
@@ -26,7 +29,6 @@ $lang['title']                 = 'Titel van het document';
 $lang['addpage']               = 'Pagina toevoegen';
 $lang['removepage']            = 'Verwijder pagina';
 $lang['showbook']              = 'Toon boek ';
-$lang['js']['sortable']              = 'Versleep de pagina\'s om ze te sorteren, verwijderen en weer toe te voegen';
 $lang['pages']                 = 'pagina/s';
 $lang['help']                  = 'Help';
 $lang['toolbar']               = 'Book creator';
@@ -37,11 +39,14 @@ $lang['deleted']               = 'Succesvol verwijderd: %s';
 $lang['duplicate']             = '&lt;De lijst met opgeslagen selecties is al weergegeven op deze pagina&gt;';
 $lang['save']                  = 'Opslaan';
 $lang['listselections']        = 'Opgeslagen Selecties';
-$lang['js']['showpage']              = 'Toon pagina';
-$lang['delselection']          = 'Verwijder opgeslagen selectie';
 $lang['showselection']         = 'Toon opgeslagen selectie';
+$lang['delselection']          = 'Verwijder opgeslagen selectie';
 $lang['loadselection']         = 'Laad opgeslagen selectie';
-$lang['titlepreparedownload']  = "Download Voorbereiden...";
-$lang['preparingdownload']     = "We zijn uw download aan het voorbereiden, even geduld alstublieft...";
-$lang['titleerrordownload']    = "Fout";
-$lang['faileddownload']        = "Er was een probleem bij het genereren van uw download.";
+$lang['selectionstored']       = 'Selectie opgeslagen';
+$lang['selectiondeleted']      = 'Selectie verwijderd';
+$lang['selectiondontexist']    = 'Selectie bestaat niet op pagina \'%s\'';
+$lang['selectionforbidden']    = 'U hebt geen toegang tot de selectie op pagina \'%s\'';
+$lang['titlepreparedownload']  = 'Download Voorbereiden...';
+$lang['preparingdownload']     = 'We zijn uw download aan het voorbereiden, even geduld alstublieft...';
+$lang['titleerrordownload']    = 'Fout';
+$lang['faileddownload']        = 'Er was een probleem bij het genereren van uw download.';
