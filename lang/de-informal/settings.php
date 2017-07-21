38ff5532a39d1a38c97c4194b8f845185f75aa1e
Felix Müller-Donath <j.felix@mueller-donath.de>
2017-07-21 14:50:05 +0200

    translation update

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Felix Müller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['toolbar']               = 'Toolbar anzeigen';
+$lang['toolbar_o_never']       = 'Nie';
+$lang['toolbar_o_always']      = 'Immer';
+$lang['toolbar_o_noempty']     = 'Wenn etwas in der Auswahl ist';
+$lang['save_namespace']        = 'In diesem Namensraum werden Auswahlen gespeichert';
+$lang['book_page']             = 'Wiki-Seite, auf der das Buch angezeigt wird';
+$lang['help_page']             = 'Wiki-Seite mit Infos und Anleitung';
+$lang['skip_ids']              = 'Bei diesen Seiten/IDs Toolbar nicht anzeigen (getrennt durch Komma)';
