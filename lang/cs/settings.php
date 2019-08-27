04d4e154ca91767ea078f4bf9fa92625638fd5ad
Eva Cerniňáková <cerninakova@gmail.com>
2019-08-27 11:20:35 +0200

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * Czech language strings for the BookCreator Plugin
  *
- * @author  Jakub Jirutka <jakub@jirutka.cz>
+ * @author Jakub Jirutka <jakub@jirutka.cz>
  */
-
-$lang['toolbar']             = 'Zobrazit panel nástrojů';
-$lang['toolbar_o_never']     = "Nikdy";
-$lang['toolbar_o_always']    = "Vždy";
-$lang['toolbar_o_noempty']   = "Pokud výběr není prázdný";
-
-$lang['save_namespace']      = 'Jmenný prostor pro uložené výběry (knihy)';
-
-$lang['book_page']           = 'Stránka knihy (musí obsahovat ~~BOOK~~)';
-$lang['help_page']           = 'Stránka nápovědy';
-
-$lang['skip_ids']            = 'Nezobrazovat panel nástrojů, pokud ID stránky obsahuje';
+$lang['toolbar']               = 'Zobrazit panel nástrojů';
+$lang['toolbar_o_never']       = 'Nikdy';
+$lang['toolbar_o_always']      = 'Vždy';
+$lang['toolbar_o_noempty']     = 'Pokud výběr není prázdný';
+$lang['save_namespace']        = 'Jmenný prostor pro uložené výběry (knihy)';
+$lang['book_page']             = 'Stránka knihy (musí obsahovat ~~BOOK~~)';
+$lang['help_page']             = 'Stránka nápovědy';
+$lang['skip_ids']              = 'Nezobrazovat panel nástrojů, pokud ID stránky obsahuje';
