1cfc64b27dfe65c809bb360bddbef9eac33605d3
Jacob Palm <jacobpalmdk@icloud.com>
2020-05-22 00:31:02 +0200

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jacob Palm <jacobpalmdk@icloud.com>
  * @author Søren Birk <soer9648@eucl.dk>
  */
 $lang['empty']                 = 'Du har endnu ikke valgt nogle sider.';
@@ -11,6 +12,8 @@ $lang['removed']               = 'Sider fjernet fra valgte';
 $lang['reset']                 = 'Ryd valgte';
 $lang['js']['include']         = 'Føj til valgte';
 $lang['js']['remove']          = 'Fjern side fra valgte';
+$lang['js']['sortable']        = 'Hiv og slip for at sortere/fjerne/tilføje sider';
+$lang['js']['showpage']        = 'Vis side';
 $lang['js']['confirmdel']      = 'Vil du virkelig slette disse valgte fra arkivet?';
 $lang['js']['confirmload']     = 'Vil du virkelig indlæse dette valg, og dermed erstatte allerede valgte sider?';
 $lang['js']['btn_addtobook']   = 'Føj til bog';
@@ -18,14 +21,15 @@ $lang['js']['btn_removetobook'] = 'Fjern fra bog';
 $lang['export']                = 'Eksportér valgte';
 $lang['create']                = 'Eksportér';
 $lang['exportpdf']             = 'Eksportér valgte til PDF';
+$lang['exportodt']             = 'Eksporter markering til ODT';
 $lang['exportprint']           = 'Udskriftsvenlig version';
 $lang['exporttext']            = 'Udelukkende tekst';
 $lang['title']                 = 'Dokumentets titel';
+$lang['skipforbiddenpages']    = 'Spring forbudte sider over';
 $lang['addpage']               = 'Føj denne side til din bog';
 $lang['removepage']            = 'Fjern denne side fra din bog';
 $lang['showbook']              = 'Vis, redigér bog';
-$lang['js']['sortable']              = 'Hiv og slip for at sortere/fjerne/tilføje sider';
-$lang['pages']                 = 'Side/r';
+$lang['pages']                 = 'Side(r)';
 $lang['help']                  = 'Hjælp';
 $lang['toolbar']               = 'Bogskaber';
 $lang['saveselection']         = 'Gem valg';
@@ -35,7 +39,14 @@ $lang['deleted']               = 'Valg fjernet med succes: %s';
 $lang['duplicate']             = '&lt;Listen med gemte valg vises allerede på denne side&gt;';
 $lang['save']                  = 'Gem';
 $lang['listselections']        = 'Liste over Valg gemt';
-$lang['js']['showpage']              = 'Vis side';
-$lang['delselection']          = 'Fjern gemt valg';
 $lang['showselection']         = 'Vis gemt valg';
+$lang['delselection']          = 'Fjern gemt valg';
 $lang['loadselection']         = 'Indlæs gemt valg';
+$lang['selectionstored']       = 'Markering gemt';
+$lang['selectiondeleted']      = 'Markering slettet';
+$lang['selectiondontexist']    = 'Markering findes ikke på siden \'%s\'';
+$lang['selectionforbidden']    = 'Du har ikke adgang til markeringen på siden \'%s\'';
+$lang['titlepreparedownload']  = 'Forbereder download...';
+$lang['preparingdownload']     = 'Vi gør klar til din download, vent venligst...';
+$lang['titleerrordownload']    = 'Fejl';
+$lang['faileddownload']        = 'Der opstod et problem under klargøring af din download';
