2a058f7245c07fc52f78c54b0762df9efded8173
Michael Große <mic.grosse@googlemail.com>
2021-01-27 09:53:28 +0100

    test: Add tests for syntax and add CI
    This is also the proof of concept for dokufreaks/dokuwiki-plugin-wizard#8

---
+++ _test/SyntaxTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dokuwiki\plugin\bookcreator\test;
+
+use Doku_Handler;
+use DokuWikiTest;
+use syntax_plugin_bookcreator;
+
+/**
+ * Tests for the syntax component of the bookcreator plugin
+ *
+ * @group plugin_bookcreator
+ * @group plugins
+ */
+class SyntaxTest extends DokuWikiTest
+{
+
+    public function syntaxProvider() {
+        return [
+            'archive syntax with full normal details' => [ '~~ARCHIVEBOOK:5&date~~', [ 'archive', 5, 'date' ] ],
+            'archive syntax with full reversed details' => [ '~~ARCHIVEBOOK:title&7~~', [ 'archive', 7, 'title' ] ],
+            'archive syntax with count only' => [ '~~ARCHIVEBOOK:8~~', [ 'archive', 8, 'date' ] ],
+            'archive syntax with order pnly' => [ '~~ARCHIVEBOOK:title~~', [ 'archive', 10, 'title' ] ],
+            'archive syntax with defaults' => [ '~~ARCHIVEBOOK~~', [ 'archive', 10, '' ] ],
+            'bookmanager syntx' => [ '~~BOOK~~', [ 'bookmanager', 10, 'date' ] ],
+        ];
+    }
+
+    /**
+     * @dataProvider syntaxProvider
+     */
+    public function test_ArchiveSyntax($syntax, $expectedData) {
+        $dokuHandler = new Doku_Handler();
+        $syntaxComponent = new syntax_plugin_bookcreator();
+
+        $result = $syntaxComponent->handle(
+            $syntax,
+            5,
+            1,
+            $dokuHandler
+        );
+
+        self::assertEquals( $expectedData, $result );
+    }
+}
