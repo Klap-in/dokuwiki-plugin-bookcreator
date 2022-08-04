0afcd2dfd2a10aa3e6f1b24ce1e9f754e43c00df
Hella <hella.breitkopf@gmail.com>
2022-08-04 23:05:07 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,12 +3,13 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hella <hella.breitkopf@gmail.com>
  */
 $lang['toolbar']               = 'Toolbar anzeigen';
 $lang['toolbar_o_never']       = 'Nie';
 $lang['toolbar_o_always']      = 'Immer';
-$lang['toolbar_o_noempty']     = 'Wenn etwas in der Auswahl ist';
+$lang['toolbar_o_noempty']     = 'Wenn die Auswahl nicht leer ist';
 $lang['save_namespace']        = 'In diesem Namensraum werden Auswahlen gespeichert';
 $lang['book_page']             = 'Wiki-Seite, auf der das Buch angezeigt wird';
 $lang['help_page']             = 'Wiki-Seite mit Infos und Anleitung';
-$lang['skip_ids']              = 'Bei diesen Seiten/IDs Toolbar nicht anzeigen (getrennt durch Komma)';
+$lang['skip_ids']              = 'Bei diesen Seiten/IDs die Werkzeugleiste nicht anzeigen (getrennt durch Komma)';
