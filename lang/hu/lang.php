7bf248d3d6e1cf7d75c39145bd16b2eb77ec4d78
Károly Gaál <karoly.gaal@europe-motion.com>
2025-09-17 19:10:13 +0200

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Károly Gaál <karoly.gaal@europe-motion.com>
+ */
+$lang['empty']                 = 'Még nincsenek kiválasztott oldalak.';
+$lang['toprint']               = 'Kiválasztott oldalak';
+$lang['removed']               = 'A kijelölésből eltávolított oldalak';
+$lang['reset']                 = 'Kijelölés törlése';
+$lang['js']['include']         = 'Hozzáadás a kiválasztáshoz';
+$lang['js']['remove']          = 'Oldal eltávolítása a kijelölésből';
+$lang['js']['sortable']        = 'Húzd és vidd módszerrel rendezze / távolítsa el / adja hozzá az oldalakat';
+$lang['js']['showpage']        = 'Oldal megjelenítése';
+$lang['js']['confirmdel']      = 'Biztosan törölni szeretné ezt a kijelölést az archívumból?';
+$lang['js']['confirmload']     = 'Biztosan betölti ezt a kijelölést, és lecseréli a már kijelölt oldalakat?';
+$lang['js']['btn_addtobook']   = 'Hozzáadás a könyvhöz';
+$lang['js']['btn_removetobook'] = 'Eltávolítás a könyvből';
+$lang['js']['namespace_picker'] = 'Névtárhely választó';
+$lang['js']['select_namespace'] = 'Válassza ki a névtárhelyet:';
+$lang['js']['added_pages']     = 'Ezeket az oldalakat adtuk hozzá:';
+$lang['js']['no_pages_selected'] = 'Nincs. 
+(Üres a névtárhely?)';
+$lang['js']['select']          = 'Válasszon';
+$lang['js']['cancel']          = 'Mégsem';
+$lang['js']['add_subns_too']   = 'Oldalak hozzáadása az al névtárhelyen is?';
+$lang['export']                = 'Kijelölés exportálása';
+$lang['create']                = 'Exportálás';
+$lang['exportpdf']             = 'Kijelölés exportálása PDF-be';
+$lang['exportodt']             = 'Kijelölés exportálása ODT-be';
+$lang['exportprint']           = 'Nyomtatható verzió';
+$lang['exporttext']            = 'Csak szöveg';
+$lang['title']                 = 'Dokumentum címe';
+$lang['skipforbiddenpages']    = 'Tiltott oldalak kihagyása';
+$lang['addpage']               = 'Add hozzá ezt az oldalt a könyvhöz';
+$lang['removepage']            = 'Távolítsa el ezt az oldalt a könyvből';
+$lang['showbook']              = 'Könyv kezelése';
+$lang['pages']                 = 'oldal (oldalak)';
+$lang['help']                  = 'Segítség';
+$lang['toolbar']               = 'Könyv készítő';
+$lang['saveselection']         = 'Kijelölés mentése';
+$lang['needtitle']             = 'Kérjük, adjon meg egy címet';
+$lang['saved']                 = 'A kijelölés sikeresen mentve: %s';
+$lang['deleted']               = 'Kijelölés sikeresen eltávolítva: %s';
+$lang['duplicate']             = '&lt;A mentett kiválasztások listája már megjelenik ezen az oldalon&gt;';
+$lang['save']                  = 'Mentés';
+$lang['listselections']        = 'Mentett kiválasztási listák';
+$lang['showselection']         = 'Mentett kijelölés megjelenítése';
+$lang['delselection']          = 'Mentett kijelölés eltávolítása';
+$lang['loadselection']         = 'Mentett kijelölés betöltése';
+$lang['selectionstored']       = 'Kijelölés tárolva';
+$lang['selectiondeleted']      = 'Kijelölés törölve';
+$lang['selectiondontexist']    = 'Nincs kijelölés ezen az oldalon \'%s\'';
+$lang['selectionforbidden']    = 'Nincs hozzáférése a kiválasztáshoz ezen az oldalon \'%s\'';
+$lang['exportselection']       = 'A mentett kijelölés exportálása \'%s\' as %s';
+$lang['export_htmlns']         = 'Névtárhely "%s:" exportálása a képernyőre HTML formátumban';
+$lang['export_textns']         = 'Névtárhely "%s:" exportálása szövegként a képernyőre';
+$lang['titlepreparedownload']  = 'Letöltés előkészítése...';
+$lang['preparingdownload']     = 'Előkészítjük a letöltést, kérjük várjon...';
+$lang['titleerrordownload']    = 'Hiba';
+$lang['faileddownload']        = 'Hiba történt a letöltés generálásakor.';
+$lang['forbidden']             = 'Nincs hozzáférésed ezekhez az oldalakhoz: %s.<br/><br/>Használja a „Tiltott oldalak kihagyása” opciót, hogy a könyv az elérhető oldalakkal készüljön.';
+$lang['select_namespace']      = 'Névtárhely hozzáadása...';
