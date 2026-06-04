721b06bd3fa699818d55c653baeb2b378025d28f
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:16:14 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['toolbar']               = 'Zobraziť panel nástrojov';
+$lang['toolbar_o_never']       = 'Nikdy';
+$lang['toolbar_o_always']      = 'Vždy';
+$lang['toolbar_o_noempty']     = 'Keď výber nie je prázdny';
+$lang['save_namespace']        = 'Menný priestor, kam sa ukladajú výbery';
+$lang['book_page']             = 'Wiki stránka, na ktorej sa zobrazuje správca kníh';
+$lang['help_page']             = 'Wiki stránka s informáciami a pokynmi';
+$lang['skip_ids']              = 'Nezobrazovať nástroje, keď ID obsahuje';
